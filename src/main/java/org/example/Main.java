144000e60f89ab85aadc793dc7395f8312471297
Zsolt Harmath <harmathzs@gmail.com>
2025-09-18 12:29:31 +0200

    Add TesztversenyHandler class

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    private static TesztversenyHandler handler;
+
+    private static void debugger() {
+        int i=0;
+    }
+
+    public static void main(String[] args) {
+        handler = new TesztversenyHandler();
+
+        System.out.println("1. feladat: Az adatok beolvasása");
+        handler.readFile("valaszok.txt");
+        debugger();
+    }
+}
