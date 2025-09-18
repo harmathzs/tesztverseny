27e47ad081f7a4b3fe5e63fca454a9f3946bec13
Zsolt Harmath <harmathzs@gmail.com>
2025-09-18 13:54:54 +0200

    Solve task 6

--- src/main/java/org/example/TesztversenyHandler.java
+++ src/main/java/org/example/TesztversenyHandler.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -129,7 +126,12 @@ public class TesztversenyHandler {
         }
     }
 
-    public void pointsToFile(String filename) {
-
+    public void pointsToFile(String filename) throws IOException {
+        File fki = new File(filename);
+        FileWriter fwki = new FileWriter(fki);
+        for (int i=0; i<getValaszokDb(); i++) {
+            fwki.write(valaszok[i][0]+";"+valaszok[i][2]+"\n");
+        }
+        fwki.close();
     }
 }
