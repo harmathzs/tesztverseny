69d3555d2e899ec4ecf8139179431102d0aa586b
Zsolt Harmath <harmathzs@gmail.com>
2025-09-18 12:48:12 +0200

    Move valaszok.txt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,7 +13,7 @@ public class Main {
         handler = new TesztversenyHandler();
 
         System.out.println("1. feladat: Az adatok beolvasása");
-        handler.readFile("valaszok.txt");
+        handler.readFile("sourceFiles/valaszok.txt");
         System.out.println();
 
         debugger();
