a7e0f15d4cbc9fed87553394040d0bd356da5499
Zsolt Harmath <harmathzs@gmail.com>
2025-09-18 14:15:41 +0200

    Solve task 7

--- src/main/java/org/example/TesztversenyHandler.java
+++ src/main/java/org/example/TesztversenyHandler.java
@@ -151,5 +151,15 @@ public class TesztversenyHandler {
                 System.out.println("1. díj ("+points1+" pont): "+valaszok[i][0]);
             }
         }
+        for (int i=0; i<getValaszokDb(); i++) {
+            if (Integer.parseInt(valaszok[i][2])==points2) {
+                System.out.println("2. díj ("+points2+" pont): "+valaszok[i][0]);
+            }
+        }
+        for (int i=0; i<getValaszokDb(); i++) {
+            if (Integer.parseInt(valaszok[i][2])==points3) {
+                System.out.println("3. díj ("+points3+" pont): "+valaszok[i][0]);
+            }
+        }
     }
 }
